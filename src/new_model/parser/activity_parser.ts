ccc139c1558dffca53b976bfca41c13543f9cbcc
ElRaptorus <christian.werner@5minds.de>
2018-04-17 12:49:25 +0200

    Implement parsing of manual tasks and script tasks

--- src/new_model/parser/activity_parser.ts
+++ src/new_model/parser/activity_parser.ts
@@ -18,12 +18,53 @@ export function parseActivitiesFromProcessData(processData: any): Array<Model.Ac
 
 function parseManualTasks(processData: any): Array<Model.Activities.ManualTask> {
 
-  return new Array<Model.Activities.ManualTask>();
+  const manualTasks: Array<Model.Activities.ManualTask> = [];
+
+  const manualTasksRaw: Array<Model.Activities.ManualTask> = getModelPropertyAsArray(processData, BpmnTags.TaskElement.ManualTask);
+
+  if (!manualTasksRaw || manualTasksRaw.length === 0) {
+    return [];
+  }
+
+  manualTasksRaw.forEach((manualTaskRaw: any): void => {
+    const manualTask: Model.Activities.ManualTask = createObjectWithCommonProperties(manualTaskRaw, Model.Activities.ManualTask);
+
+    manualTask.incoming = getModelPropertyAsArray(manualTaskRaw, BpmnTags.FlowElementProperty.SequenceFlowIncoming);
+    manualTask.outgoing = getModelPropertyAsArray(manualTaskRaw, BpmnTags.FlowElementProperty.SequenceFlowOutgoing);
+
+    manualTask.name = manualTaskRaw.name;
+
+    manualTasks.push(manualTask);
+  });
+
+  return manualTasks;
 }
 
 function parseScriptTasks(processData: any): Array<Model.Activities.ScriptTask> {
 
-  return new Array<Model.Activities.ScriptTask>();
+  const scriptTasks: Array<Model.Activities.ScriptTask> = [];
+
+  const scriptTasksRaw: Array<Model.Activities.ScriptTask> = getModelPropertyAsArray(processData, BpmnTags.TaskElement.ManualTask);
+
+  if (!scriptTasksRaw || scriptTasksRaw.length === 0) {
+    return [];
+  }
+
+  scriptTasksRaw.forEach((scriptTaskRaw: any): void => {
+    const scriptTask: Model.Activities.ScriptTask = createObjectWithCommonProperties(scriptTaskRaw, Model.Activities.ScriptTask);
+
+    scriptTask.incoming = getModelPropertyAsArray(scriptTaskRaw, BpmnTags.FlowElementProperty.SequenceFlowIncoming);
+    scriptTask.outgoing = getModelPropertyAsArray(scriptTaskRaw, BpmnTags.FlowElementProperty.SequenceFlowOutgoing);
+
+    scriptTask.name = scriptTaskRaw.name;
+    scriptTask.scriptFormat = scriptTaskRaw.scriptFormat;
+    scriptTask.script = scriptTaskRaw[BpmnTags.FlowElementProperty.BpmnScript];
+    scriptTask.resultVariable = scriptTaskRaw[BpmnTags.FlowElementProperty.CamundaResultVariable];
+
+    scriptTasks.push(scriptTask);
+  });
+
+  return scriptTasks;
 }
 
 function parseServiceTasks(processData: any): Array<Model.Activities.ServiceTask> {
