ade2f80030cf5db42072b9292e71c867c03eac1c
5ebastianMeier <sebastian.meier@5minds.de>
2018-05-09 13:56:29 +0200

    update interface for process execution service

--- src/execution/iexecute_process_service.ts
+++ src/execution/iexecute_process_service.ts
@@ -1,6 +1,7 @@
 import { ExecutionContext } from "@essential-projects/core_contracts";
 import { IProcessDefEntity, IProcessEntity } from '@process-engine/process_engine_contracts';
+import { Model } from '@process-engine/process_engine_contracts';
 
 export interface IExecuteProcessService {
-    start(context: ExecutionContext, processDefinition: IProcessDefEntity, processInstance: IProcessEntity): void;
+    start(context: ExecutionContext, processDefinition: Model.Types.Process): void;
 }
