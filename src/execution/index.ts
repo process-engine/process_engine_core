50de1b5e5e3f7fa5f75d02cb836811288ee2885f
5ebastianMeier <sebastian.meier@5minds.de>
2018-05-15 10:38:01 +0200

    add export for process token fascade

--- src/execution/index.ts
+++ src/execution/index.ts
@@ -3,3 +3,4 @@ export * from './execute_process_service';
 export * from './next_flow_node_info';
 export * from './handler';
 export * from './process_model_fascade';
+export * from './process_token_fascade';
