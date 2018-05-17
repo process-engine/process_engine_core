e3327479d5aaeb01d5474548504a314b68df81ba
ElRaptorus <christian.werner@5minds.de>
2018-05-17 16:38:55 +0200

    Use Consumer Api Service to perform call activities

--- src/entity_types/subprocess_external.ts
+++ src/entity_types/subprocess_external.ts
@@ -1,7 +1,15 @@
-import {IConsumerApiService} from '@process-engine/consumer_api_contracts';
+import {ConsumerContext,
+        IConsumerApiService,
+        ProcessModel,
+        ProcessStartRequestPayload,
+        ProcessStartResponsePayload,
+        StartCallbackType,
+} from '@process-engine/consumer_api_contracts';
+
 import {ExecutionContext, IEntity, IInheritedSchema} from '@essential-projects/core_contracts';
 import {EntityDependencyHelper, IEntityType, IPropertyBag} from '@essential-projects/data_model_contracts';
 import {IParamStart, IProcessDefEntityTypeService, ISubprocessExternalEntity} from '@process-engine/process_engine_contracts';
+
 import {NodeInstanceEntity, NodeInstanceEntityDependencyHelper} from './node_instance';
 
 import * as debug from 'debug';
@@ -41,48 +49,60 @@ export class SubprocessExternalEntity extends NodeInstanceEntity implements ISub
 
   public async execute(context: ExecutionContext): Promise<void> {
     const internalContext = await this.iamService.createInternalContext('processengine_system');
-    this.state = 'wait';
+    this.state = 'progress';
 
     if (this.process.processDef.persist) {
       await this.save(internalContext, { reloadAfterSave: false });
     }
 
-    // call sub process
     const subProcessKey = this.nodeDef.subProcessKey || null;
-    if (subProcessKey) {
-      debugInfo(`Executing Call activity with process model key '${this.key}'`);
-
-      let initialToken: any;
-  
-      if (this.processToken && this.processToken.data) {
-        initialToken = this.processToken.data.current || {};
-      }
-
-      const params: IParamStart = {
-        key: subProcessKey,
-        source: this,
-        isSubProcess: true,
-        initialToken: initialToken,
-        participant: this.participant,
-      };
-
-      const subProcessRef = await this.processDefEntityTypeService.start(context, params);
-      this.process.boundProcesses[subProcessRef.id] = subProcessRef;
-
-    } else {
+    if (!subProcessKey) {
       debugInfo(`No key is provided for call activity key '${this.key}'`);
-    }
+      this.changeState(context, 'end', this);
+      return;
+    } 
+    
+    debugInfo(`Executing Call activity '${this.key}', using subprocess key '${subProcessKey}'`);
 
-  }
-
-  public async proceed(context: ExecutionContext, newData: any, source: IEntity, applicationId: string, participant: string): Promise<void> {
+    const result: ProcessStartResponsePayload = await this._executeSubProcess(context);
 
     // save new data in token
-    const processToken = this.processToken;
-    const tokenData = processToken.data || {};
-    tokenData.current = newData;
-    processToken.data = tokenData;
+    const tokenData = this.processToken.data || {};
+    tokenData.current = result;
+    this.processToken.data = tokenData;
 
     this.changeState(context, 'end', this);
+  }
+
+  private async _executeSubProcess(context: ExecutionContext): Promise<ProcessStartResponsePayload> {
+
+    const consumerContext: ConsumerContext = {
+      identity: context.encryptedToken
+    };
+
+    const startEventKey: string = await this._getAccessibleStartEventForProcessModel(consumerContext, this.nodeDef.subProcessKey);
+
+    const payload: ProcessStartRequestPayload = {
+      correlation_id: this.id,
+      input_values: {},
+    };
+
+    const startCallbackType: StartCallbackType = StartCallbackType.CallbackOnEndEventReached;
+
+    const result: ProcessStartResponsePayload = 
+      await this.consumerApiService.startProcessInstance(consumerContext, this.nodeDef.subProcessKey, startEventKey, payload, startCallbackType);
+
+    return result;
+  }
+
+  private async _getAccessibleStartEventForProcessModel(consumerContext: ConsumerContext, subProcessKey: string): Promise<string> {
+
+    const processModel: ProcessModel = await this.consumerApiService.getProcessModelByKey(consumerContext, subProcessKey);
+
+    // Pick the first accessible start event
+    // Note: If no start events are accessible by the user, the consume api will already have thrown an error.
+    const startEventKey: string = processModel.startEvents[0].key;
+
+    return startEventKey;
   }
 }
