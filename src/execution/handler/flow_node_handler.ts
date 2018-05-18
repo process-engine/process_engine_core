88118454ea0f14cffb61262cf53dce1524415d4a
5ebastianMeier <sebastian.meier@5minds.de>
2018-05-18 11:10:05 +0200

    clean up obsolete comments

--- src/execution/handler/flow_node_handler.ts
+++ src/execution/handler/flow_node_handler.ts
@@ -5,9 +5,6 @@ import { IFlowNodeHandler, IFlowNodeHandlerFactory, IProcessModelFascade, NextFl
 
 export abstract class FlowNodeHandler<TFlowNode extends Model.Base.FlowNode> implements IFlowNodeHandler<TFlowNode> {
 
-    constructor() {
-    }
-
     public async execute(flowNode: TFlowNode, processTokenFascade: IProcessTokenFascade, processModelFascade: IProcessModelFascade): Promise<NextFlowNodeInfo> {
 
         let nextFlowNode: NextFlowNodeInfo;
@@ -20,6 +17,10 @@ export abstract class FlowNodeHandler<TFlowNode extends Model.Base.FlowNode> imp
             await processTokenFascade.addResultForFlowNode(flowNode.id, error);
         }
 
+        if (!nextFlowNode) {
+            throw new Error(`Next flow node after node with id "${flowNode.id}" could not be found.`);
+        }
+
         await this.afterExecute(flowNode, nextFlowNode.flowNode, processTokenFascade, processModelFascade);
 
         return nextFlowNode;
@@ -27,36 +28,9 @@ export abstract class FlowNodeHandler<TFlowNode extends Model.Base.FlowNode> imp
 
     protected async abstract executeIntern(flowNode: TFlowNode, processTokenFascade: IProcessTokenFascade, processModelFascade: IProcessModelFascade): Promise<NextFlowNodeInfo>;
 
-    // protected async getNextFlowNodeFor(flowNode: Model.Base.FlowNode, context: ExecutionContext): Promise<Model.Base.FlowNode> {
-
-    //     const processDefinition: Model.Types.Process = await flowNode.getProcessDef(context);
-    //     const nextFlowNodeId: string = await this.getNextFlowNodeId(flowNode.id, processDefinition, context);
-
-    //     return this.getFlowNodeById(nextFlowNodeId, processDefinition);
-    // }
-
-    // protected getFlowNodeById(flowNodeId: string, processDefinition: Model.Types.Process): Model.Base.FlowNode {
-    //     const nextFlowNode: Model.Base.FlowNode = processDefinition.nodeDefCollection.data.find((currentFlowNode: Model.Base.FlowNode) => {
-    //         return currentFlowNode.id === flowNodeId;
-    //     });
-
-    //     return nextFlowNode;
-    // }
-
-    // private async getNextFlowNodeId(flowNodeId: string, processDefinition: Model.Types.Process, context: ExecutionContext): Promise<string> {
-
-    //     const flow: IFlowDefEntity = processDefinition.flowDefCollection.data.find((sequenceFlow: IFlowDefEntity) => {
-    //         return sequenceFlow.source.id === flowNodeId;
-    //     });
-
-    //     if (flow === undefined) {
-    //         return null;
-    //     }
-
-    //     return flow.target.id;
-    // }
-
     private async afterExecute(flowNode: TFlowNode, nextFlowNode: Model.Base.FlowNode, processTokenFascade: IProcessTokenFascade, processModelFascade: IProcessModelFascade): Promise<void> {
+
+        await processTokenFascade.evaluateMapperForFlowNode(flowNode);
 
         const nextSequenceFlow: Model.Types.SequenceFlow = processModelFascade.getSequenceFlowBetween(flowNode, nextFlowNode);
 
@@ -64,30 +38,6 @@ export abstract class FlowNodeHandler<TFlowNode extends Model.Base.FlowNode> imp
             return;
         }
 
-        await processTokenFascade.evaluateMapper(nextSequenceFlow);
-        // const tokenData: any = await processTokenFascade.getOldTokenFormat();
-
-        // const mapper: string = (flowNode as any).mapper;
-
-        // if (mapper !== undefined) {
-        //     const newCurrent = (new Function('token', 'return ' + mapper)).call(tokenData, tokenData);
-        //     tokenData.current = newCurrent;
-        // }
-
-        // tokenData.history = tokenData.history || {};
-
-        // if (!tokenData.history[flowNode.id]) {
-        //     tokenData.history[flowNode.id] = tokenData.current;
-        // } else {
-        //     if (!Array.isArray(tokenData.history[flowNode.id])) {
-        //         tokenData.history[flowNode.id] = [tokenData.history[flowNode.id]];
-        //     }
-
-        //     tokenData.history[flowNode.id].push(tokenData.current);
-        // }
-
-        // processToken.data = tokenData;
-
-        // await this.processEngineStorageService.saveProcessToken(processToken);
+        await processTokenFascade.evaluateMapperForSequenceFlow(nextSequenceFlow);
     }
 }
