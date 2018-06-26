e3818e0bab20a8ea90f6a6661ad5e7528ed3a877
5ebastianMeier <sebastian.meier@5minds.de>
2018-06-26 10:03:56 +0200

    fix token handling

--- src/new_model/runtime/engine/handler/script_task_handler.ts
+++ src/new_model/runtime/engine/handler/script_task_handler.ts
@@ -59,6 +59,8 @@ export class ScriptTaskHandler extends FlowNodeHandler<Model.Activities.ScriptTa
     const nextFlowNode: Model.Base.FlowNode = await processModelFacade.getNextFlowNodeFor(scriptTask);
 
     await processTokenFacade.addResultForFlowNode(scriptTask.id, finalResult);
+    token.payload = finalResult;
+
     await this.flowNodeInstancePersistence.persistOnExit(token, scriptTask.id, flowNodeInstanceId);
 
     return new NextFlowNodeInfo(nextFlowNode, token, processTokenFacade);
