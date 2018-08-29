7c137a0a1379573a621af8c39f827c3db92e5646
Robin <robin.lenz@5minds.de>
2018-08-29 08:50:32 +0200

    TabSize changed

--- test/runtime/engine/process_model_facade.spec.ts
+++ test/runtime/engine/process_model_facade.spec.ts
@@ -10,29 +10,29 @@ import { ProcessModelFacadeTestFixture } from './process_model_facade_test_fixtu
 
 describe('ProcessModelFacade', () => {
 
-    describe('parse DemoNutztierRiss.bpmn', () => {
-        it('Should parse expected process', async () => {
+  describe('parse DemoNutztierRiss.bpmn', () => {
+    it('Should parse expected process', async () => {
 
-            const fixture = new ProcessModelFacadeTestFixture();
-            await fixture.initialize('./test/bpmns/DemoNutztierRiss.bpmn');
+      const fixture = new ProcessModelFacadeTestFixture();
+      await fixture.initialize('./test/bpmns/DemoNutztierRiss.bpmn');
 
-            await fixture.assertFlowNodes([
-                'StartEvent_1',
-                'VorgangErfassen',
-                'Task_01xg9lr',
-                'Task_00dom74',
-                'notizSchreiben',
-                'Task_1tk0lhq',
-                'Task_1yzqmfq',
-                'EndEvent_05uuvaq'
-            ]);
+      await fixture.assertFlowNodes([
+        'StartEvent_1',
+        'VorgangErfassen',
+        'Task_01xg9lr',
+        'Task_00dom74',
+        'notizSchreiben',
+        'Task_1tk0lhq',
+        'Task_1yzqmfq',
+        'EndEvent_05uuvaq'
+      ]);
 
-            const vorgangAnlegen = fixture.getFlowNodeById<Model.Activities.ServiceTask>('Task_01xg9lr');
-            const invocation: Model.Activities.MethodInvocation = vorgangAnlegen.invocation as Model.Activities.MethodInvocation;
+      const vorgangAnlegen = fixture.getFlowNodeById<Model.Activities.ServiceTask>('Task_01xg9lr');
+      const invocation: Model.Activities.MethodInvocation = vorgangAnlegen.invocation as Model.Activities.MethodInvocation;
 
-            should(invocation.module).be.eql('HttpService');
-            should(invocation.method).be.eql('post');
-            should(invocation.params).be.eql('[\'http://localhost:5000/api/vorgaenge/anlegen\', token.history.VorgangErfassen]');
-        });
-    })
+      should(invocation.module).be.eql('HttpService');
+      should(invocation.method).be.eql('post');
+      should(invocation.params).be.eql('[\'http://localhost:5000/api/vorgaenge/anlegen\', token.history.VorgangErfassen]');
+    });
+  })
 });
--- test/runtime/engine/process_model_facade_test_fixture.ts
+++ test/runtime/engine/process_model_facade_test_fixture.ts
@@ -3,47 +3,47 @@ import * as fs from 'fs';
 import * as should from 'should';
 
 // ProcessEngine/Essential Project Imports
-import { Model } from '@process-engine/process_engine_contracts';
+import {Model} from '@process-engine/process_engine_contracts';
 
 // Local imports
-import { BpmnModelParser } from '../../../src/model/bpmn_model_parser';
-import { ProcessModelFacade } from '../../../src/runtime/engine';
+import {BpmnModelParser} from '../../../src/model/bpmn_model_parser';
+import {ProcessModelFacade} from '../../../src/runtime/engine';
 
 export class ProcessModelFacadeTestFixture {
 
-    private processModelFacade: ProcessModelFacade;
+  private processModelFacade: ProcessModelFacade;
 
-    public async initialize(bpmnFilename: string): Promise<void> {
-        const parser = new BpmnModelParser();
-        parser.initialize();
+  public async initialize(bpmnFilename: string): Promise<void> {
+    const parser = new BpmnModelParser();
+    parser.initialize();
 
-        const bpmnXml = fs.readFileSync(bpmnFilename, 'utf8');
-        const definitions = await parser.parseXmlToObjectModel(bpmnXml);
-        const process = definitions.processes[0];
+    const bpmnXml = fs.readFileSync(bpmnFilename, 'utf8');
+    const definitions = await parser.parseXmlToObjectModel(bpmnXml);
+    const process = definitions.processes[0];
 
-        this.processModelFacade = new ProcessModelFacade(process);
+    this.processModelFacade = new ProcessModelFacade(process);
+  }
+
+  public async assertFlowNodes(flowNodeIds: Array<string>): Promise<void> {
+    let currentFlowNode: Model.Base.FlowNode = this.processModelFacade.getStartEvents()[0];
+    let flowNodeIdIndex = 0;
+
+    while (this.hasNextNextFlowNode(currentFlowNode)) {
+
+      should(flowNodeIds[flowNodeIdIndex]).be.eql(currentFlowNode.id);
+
+      currentFlowNode = this.processModelFacade.getNextFlowNodeFor(currentFlowNode);
+      flowNodeIdIndex++;
     }
 
-    public async assertFlowNodes(flowNodeIds: Array<string>): Promise<void> {
-        let currentFlowNode: Model.Base.FlowNode = this.processModelFacade.getStartEvents()[0];
-        let flowNodeIdIndex = 0;
+    should(flowNodeIdIndex).be.eql(flowNodeIds.length);
+  }
 
-        while (this.hasNextNextFlowNode(currentFlowNode)) {
+  public getFlowNodeById<TFlowNode extends Model.Base.FlowNode>(id: string): TFlowNode {
+    return this.processModelFacade.getFlowNodeById(id) as TFlowNode;
+  }
 
-            should(flowNodeIds[flowNodeIdIndex]).be.eql(currentFlowNode.id);
-
-            currentFlowNode = this.processModelFacade.getNextFlowNodeFor(currentFlowNode);
-            flowNodeIdIndex++;
-        }
-
-        should(flowNodeIdIndex).be.eql(flowNodeIds.length);
-    }
-
-    public getFlowNodeById<TFlowNode extends Model.Base.FlowNode>(id: string): TFlowNode {
-        return this.processModelFacade.getFlowNodeById(id) as TFlowNode;
-    }
-
-    private hasNextNextFlowNode(flowNode: Model.Base.FlowNode): boolean {
-        return flowNode != null;
-    }
+  private hasNextNextFlowNode(flowNode: Model.Base.FlowNode): boolean {
+    return flowNode != null;
+  }
 }
