c594735a9e49f516dcebd295ebf4a9b0cc2a9c8c
ElRaptorus <christian.werner@5minds.de>
2018-08-29 18:21:24 +0200

    :recycle: Add filter for empty camunda properties to model parser

--- src/model/type_factory.ts
+++ src/model/type_factory.ts
@@ -47,16 +47,71 @@ export function setCommonObjectPropertiesFromData(data: any, instance: Model.Bas
 
     // NOTE: The extension property collection is wrapped in a property named "camunda:property",
     // which in turn is located in "camunda:properties".
-    const propertyCollection: any = extensionData[BpmnTags.CamundaProperty.Properties];
+    let camundaProperties: any = extensionData[BpmnTags.CamundaProperty.Properties];
 
-    if (propertyCollection !== undefined) {
+    if (camundaProperties !== undefined) {
+
+      camundaProperties = filterOutEmptyProperties(camundaProperties);
 
       // This covers all properties defined in the Extensions-Panel (mapper, module/method/param, etc).
       instance.extensionElements.camundaExtensionProperties =
-        getModelPropertyAsArray(propertyCollection, BpmnTags.CamundaProperty.Property);
+        getModelPropertyAsArray(camundaProperties, BpmnTags.CamundaProperty.Property);
     }
 
   }
 
   return instance;
+}
+
+/**
+ * This is supposed to address the issue, where empty camunda:property tags will cause
+ * unexpected behavior when executing a process model.
+ * For instance, a service task's invocation would not be usable, or a UserTask's FormFields could
+ * not be addressed.
+ *
+ * This function takes a list of camunda properties and filters out all empty ones.
+ * Depending on what is left afterwards, we will get either a single value, an Array, or nothing.
+ *
+ * @param camundaProperties The property list to filter.
+ */
+function filterOutEmptyProperties(camundaProperties: any): any {
+
+  if (!Array.isArray(camundaProperties)) {
+    return camundaProperties;
+  }
+
+  const filteredProperties: Array<any> = camundaProperties.filter((property: any): boolean => {
+    const isNotEmpty: boolean = property !== undefined;
+
+    let hasValue: boolean = false;
+
+    if (typeof property === 'string') {
+      hasValue = isNotEmpty && property.length > 0;
+    } else if (typeof property === 'object') {
+      hasValue = isNotEmpty && Object.keys(property).length > 0;
+    } else if (Array.isArray(property)) {
+      hasValue = isNotEmpty && property.length > 0;
+    }
+
+    return hasValue;
+  });
+
+  if (filteredProperties.length === 0) {
+    // No properties were declared, so don't bother returning anything.
+    return undefined;
+  }
+
+  if (filteredProperties.length === 1) {
+    // Usually, when only a single property is declared on an element,
+    //the parsed result would look something like this:
+    //
+    // { 'camunda:properties': 'someValue' }
+    //
+    // Since we have an Array here, we need to return that value specifically,
+    // in order to keep the ProcessModel's structure intact.
+    return filteredProperties[0];
+  }
+
+  // More than one property was declared, so return the Array as it is.
+  return filteredProperties;
 }
