73407fc276e2d81cf956d768255180e5a2528d97
ElRaptorus <christian.werner@5minds.de>
2018-09-24 10:57:50 +0200

    :fire: Remove ConsumerContext from CallActivity handler

--- src/runtime/engine/handler/call_activity_handler.ts
+++ src/runtime/engine/handler/call_activity_handler.ts
@@ -1,7 +1,6 @@
 
 import {
-  ConsumerContext,
-  IConsumerApiService,
+  IConsumerApi,
   ProcessModel,
   ProcessStartRequestPayload,
   ProcessStartResponsePayload,
@@ -24,15 +23,15 @@ import {FlowNodeHandler} from './index';
 
 export class CallActivityHandler extends FlowNodeHandler<Model.Activities.CallActivity> {
 
-  private _consumerApiService: IConsumerApiService;
+  private _consumerApiService: IConsumerApi;
 
-  constructor(consumerApiService: IConsumerApiService, flowNodeInstanceService: IFlowNodeInstanceService, metricsService: IMetricsApi) {
+  constructor(consumerApiService: IConsumerApi, flowNodeInstanceService: IFlowNodeInstanceService, metricsService: IMetricsApi) {
     super(flowNodeInstanceService, metricsService);
 
     this._consumerApiService = consumerApiService;
   }
 
-  private get consumerApiService(): IConsumerApiService {
+  private get consumerApiService(): IConsumerApi {
     return this._consumerApiService;
   }
 
@@ -46,20 +45,16 @@ export class CallActivityHandler extends FlowNodeHandler<Model.Activities.CallAc
 
     const identity: IIdentity = await executionContextFacade.getIdentity();
 
-    const consumerContext: ConsumerContext = {
-      identity: identity.token,
-    };
-
     const tokenData: any = await processTokenFacade.getOldTokenFormat();
 
     const processInstanceId: string = token.processInstanceId;
     const correlationId: string = token.correlationId;
-    const startEventId: string = await this._getAccessibleStartEvent(consumerContext, callActivity.calledReference);
+    const startEventId: string = await this._getAccessibleStartEvent(identity, callActivity.calledReference);
 
     await this.persistOnSuspend(callActivity, token);
 
     const processStartResponse: ProcessStartResponsePayload =
-      await this._waitForSubProcessToFinishAndReturnCorrelationId(consumerContext,
+      await this._waitForSubProcessToFinishAndReturnCorrelationId(identity,
                                                                   correlationId,
                                                                   processInstanceId,
                                                                   startEventId,
@@ -83,14 +78,14 @@ export class CallActivityHandler extends FlowNodeHandler<Model.Activities.CallAc
    * given ID.
    *
    * @async
-   * @param   consumerContext The users auth context.
-   * @param   processModelId  The ID of the ProcesssModel for which to get an
-   *                          accessible StartEvent.
-   * @returns                 The retrieved StartEvent.
+   * @param   identity       The users identity.
+   * @param   processModelId The ID of the ProcesssModel for which to get an
+   *                         accessible StartEvent.
+   * @returns                The retrieved StartEvent.
    */
-  private async _getAccessibleStartEvent(consumerContext: ConsumerContext, processModelId: string): Promise<string> {
+  private async _getAccessibleStartEvent(identity: IIdentity, processModelId: string): Promise<string> {
 
-    const processModel: ProcessModel = await this.consumerApiService.getProcessModelById(consumerContext, processModelId);
+    const processModel: ProcessModel = await this.consumerApiService.getProcessModelById(identity, processModelId);
 
     /*
      * Note: If the user cannot access the process model and/or its start events,
@@ -107,7 +102,7 @@ export class CallActivityHandler extends FlowNodeHandler<Model.Activities.CallAc
    * CallActivity FlowNode.
    *
    * @async
-   * @param consumerContext   The users auth context.
+   * @param identity          The users identity.
    * @param correlationId     The ID of the Correlation in which to run the
    *                          SubProcess specified in the CallActivtiy.
    * @param processInstanceId The ID of the current ProcessInstance.
@@ -116,7 +111,7 @@ export class CallActivityHandler extends FlowNodeHandler<Model.Activities.CallAc
    *                          SubProcess to start.
    * @param tokenData         The current ProcessToken.
    */
-  private async _waitForSubProcessToFinishAndReturnCorrelationId(consumerContext: ConsumerContext,
+  private async _waitForSubProcessToFinishAndReturnCorrelationId(identity: IIdentity,
                                                                  correlationId: string,
                                                                  processInstanceId: string,
                                                                  startEventId: string,
@@ -134,7 +129,7 @@ export class CallActivityHandler extends FlowNodeHandler<Model.Activities.CallAc
     const processModelId: string = callActivity.calledReference;
 
     const result: ProcessStartResponsePayload =
-      await this.consumerApiService.startProcessInstance(consumerContext, processModelId, startEventId, payload, startCallbackType);
+      await this.consumerApiService.startProcessInstance(identity, processModelId, startEventId, payload, startCallbackType);
 
     return result;
   }
