a75887880de40888be347b8502080b4ae710c022
ElRaptorus <christian.werner@5minds.de>
2018-11-20 16:47:42 +0100

    :sparkles: Implement resume functions for Send- and Receive- Task  handlers

--- src/runtime/engine/flow_node_handler/receive_task_handler.ts
+++ src/runtime/engine/flow_node_handler/receive_task_handler.ts
@@ -1,4 +1,4 @@
-import {UnprocessableEntityError} from '@essential-projects/errors_ts';
+import {InternalServerError} from '@essential-projects/errors_ts';
 import {IEventAggregator, ISubscription} from '@essential-projects/event_aggregator_contracts';
 import {IIdentity} from '@essential-projects/iam_contracts';
 import {ILoggingApi} from '@process-engine/logging_api_contracts';
@@ -38,17 +38,123 @@ export class ReceiveTaskHandler extends FlowNodeHandler<Model.Activities.Receive
                                     identity: IIdentity): Promise<NextFlowNodeInfo> {
 
     await this.persistOnEnter(token);
-
-    const noMessageDefinitionProvided: boolean = this.receiveTask.messageEventDefinition === undefined;
-    if (noMessageDefinitionProvided) {
-      throw new UnprocessableEntityError('SendTask has no MessageDefinition!');
-    }
-
     await this.persistOnSuspend(token);
+
+    return this._executeHandler(token, processTokenFacade, processModelFacade);
+  }
+
+  protected async resumeInternally(flowNodeInstance: Runtime.Types.FlowNodeInstance,
+                                   processTokenFacade: IProcessTokenFacade,
+                                   processModelFacade: IProcessModelFacade,
+                                   identity: IIdentity,
+                                  ): Promise<NextFlowNodeInfo> {
+
+    switch (flowNodeInstance.state) {
+      case Runtime.Types.FlowNodeInstanceState.suspended:
+        return this._continueAfterSuspend(flowNodeInstance, processTokenFacade, processModelFacade);
+      case Runtime.Types.FlowNodeInstanceState.running:
+
+        const resumeToken: Runtime.Types.ProcessToken =
+          flowNodeInstance.tokens.find((token: Runtime.Types.ProcessToken): boolean => {
+            return token.type === Runtime.Types.ProcessTokenType.onResume;
+          });
+
+        const receiveTaskNotYetTriggered: boolean = resumeToken === undefined;
+
+        if (receiveTaskNotYetTriggered) {
+          return this._continueAfterEnter(flowNodeInstance, processTokenFacade, processModelFacade);
+        }
+
+        return this._continueAfterResume(resumeToken, processTokenFacade, processModelFacade);
+      default:
+        throw new InternalServerError(`Cannot resume ReceiveTask instance ${flowNodeInstance.id}, because it was already finished!`);
+    }
+  }
+
+  /**
+   * Resumes the given FlowNodeInstance from the point where it assumed the
+   * "onEnter" state.
+   *
+   * Basically, the handler was not yet executed, except for the initial
+   * state change.
+   *
+   * @async
+   * @param   flowNodeInstance   The FlowNodeInstance to resume.
+   * @param   processTokenFacade The ProcessTokenFacade to use for resuming.
+   * @param   processModelFacade The processModelFacade to use for resuming.
+   * @returns                    The Info for the next FlowNode to run.
+   */
+  private async _continueAfterEnter(flowNodeInstance: Runtime.Types.FlowNodeInstance,
+                                    processTokenFacade: IProcessTokenFacade,
+                                    processModelFacade: IProcessModelFacade,
+                                   ): Promise<NextFlowNodeInfo> {
+
+    // When the FNI was interrupted directly after the onEnter state change, only one token will be present.
+    const onEnterToken: Runtime.Types.ProcessToken = flowNodeInstance.tokens[0];
+
+    return this._executeHandler(onEnterToken, processTokenFacade, processModelFacade);
+  }
+
+  /**
+   * Resumes the given FlowNodeInstance from the point where it assumed the
+   * "onSuspended" state.
+   *
+   * When the FlowNodeInstance was interrupted during this stage, we need to
+   * run the handler again, except for the "onSuspend" state change.
+   *
+   * @async
+   * @param   flowNodeInstance   The FlowNodeInstance to resume.
+   * @param   processTokenFacade The ProcessTokenFacade to use for resuming.
+   * @param   processModelFacade The processModelFacade to use for resuming.
+   * @returns                    The Info for the next FlowNode to run.
+   */
+  private async _continueAfterSuspend(flowNodeInstance: Runtime.Types.FlowNodeInstance,
+                                      processTokenFacade: IProcessTokenFacade,
+                                      processModelFacade: IProcessModelFacade,
+                                     ): Promise<NextFlowNodeInfo> {
+
+    const suspendToken: Runtime.Types.ProcessToken =
+      flowNodeInstance.tokens.find((token: Runtime.Types.ProcessToken): boolean => {
+        return token.type === Runtime.Types.ProcessTokenType.onSuspend;
+      });
+
+    return this._executeHandler(suspendToken, processTokenFacade, processModelFacade);
+  }
+
+  /**
+   * Resumes the given FlowNodeInstance from the point where it assumed the
+   * "onResumed" state.
+   *
+   * Basically, the ReceiveTask was already finished.
+   * The final result is only missing in the database.
+   *
+   * @async
+   * @param   resumeToken   The FlowNodeInstance to resume.
+   * @param   processTokenFacade The ProcessTokenFacade to use for resuming.
+   * @param   processModelFacade The processModelFacade to use for resuming.
+   * @returns                    The Info for the next FlowNode to run.
+   */
+  private async _continueAfterResume(resumeToken: Runtime.Types.ProcessToken,
+                                     processTokenFacade: IProcessTokenFacade,
+                                     processModelFacade: IProcessModelFacade,
+                                    ): Promise<NextFlowNodeInfo> {
+
+    processTokenFacade.addResultForFlowNode(this.receiveTask.id, resumeToken.payload);
+
+    const nextNodeAfter: Model.Base.FlowNode = processModelFacade.getNextFlowNodeFor(this.receiveTask);
+
+    await this.persistOnExit(resumeToken);
+
+    return new NextFlowNodeInfo(nextNodeAfter, resumeToken, processTokenFacade);
+  }
+
+  private async _executeHandler(token: Runtime.Types.ProcessToken,
+                                processTokenFacade: IProcessTokenFacade,
+                                processModelFacade: IProcessModelFacade): Promise<NextFlowNodeInfo> {
+
     const receivedMessage: MessageEventReachedMessage = await this._waitForMessage();
     await this.persistOnResume(token);
-
-    await this._sendReplyToSender(token);
+    this._sendReplyToSender(token);
 
     processTokenFacade.addResultForFlowNode(this.receiveTask.id, receivedMessage.currentToken);
     const nextFlowNodeInfo: Model.Base.FlowNode = processModelFacade.getNextFlowNodeFor(this.receiveTask);
@@ -57,15 +163,12 @@ export class ReceiveTaskHandler extends FlowNodeHandler<Model.Activities.Receive
     return new NextFlowNodeInfo(nextFlowNodeInfo, token, processTokenFacade);
   }
 
-  public async resumeInternally(flowNodeInstance: Runtime.Types.FlowNodeInstance,
-                                processTokenFacade: IProcessTokenFacade,
-                                processModelFacade: IProcessModelFacade,
-                                identity: IIdentity,
-                              ): Promise<NextFlowNodeInfo> {
-
-    throw new Error('Not implemented yet.');
-  }
-
+  /**
+   * Waits for an incoming message from a SendTask.
+   *
+   * @async
+   * @returns The received message.
+   */
   private async _waitForMessage(): Promise<MessageEventReachedMessage> {
 
     return new Promise<MessageEventReachedMessage>((resolve: Function): void => {
@@ -86,7 +189,13 @@ export class ReceiveTaskHandler extends FlowNodeHandler<Model.Activities.Receive
     });
   }
 
-  private async _sendReplyToSender(token: Runtime.Types.ProcessToken): Promise<void> {
+  /**
+   * Publishes a message to the EventAggregator, informing any SendTasks that
+   * may be listening about the receit of the message.
+   *
+   * @param token The current ProcessToken.
+   */
+  private _sendReplyToSender(token: Runtime.Types.ProcessToken): void {
 
     const messageName: string = this.receiveTask.messageEventDefinition.name;
 
--- src/runtime/engine/flow_node_handler/send_task_handler.ts
+++ src/runtime/engine/flow_node_handler/send_task_handler.ts
@@ -1,4 +1,4 @@
-import {UnprocessableEntityError} from '@essential-projects/errors_ts';
+import {InternalServerError, UnprocessableEntityError} from '@essential-projects/errors_ts';
 import {IEventAggregator, ISubscription} from '@essential-projects/event_aggregator_contracts';
 import {IIdentity} from '@essential-projects/iam_contracts';
 
@@ -37,62 +37,176 @@ export class SendTaskHandler extends FlowNodeHandler<Model.Activities.SendTask> 
                                     processTokenFacade: IProcessTokenFacade,
                                     processModelFacade: IProcessModelFacade,
                                     identity: IIdentity): Promise<NextFlowNodeInfo> {
+
     await this.persistOnEnter(token);
+    await this.persistOnSuspend(token);
 
-    const noMessageDefinitionProvided: boolean = this.sendTask.messageEventDefinition === undefined;
-    if (noMessageDefinitionProvided) {
-      throw new UnprocessableEntityError('SendTask has no MessageDefinition!');
-    }
-
-    await this._registerEventHandlerAndSendMessage(token);
-
-    const nextFlowNodeInfo: Model.Base.FlowNode = processModelFacade.getNextFlowNodeFor(this.sendTask);
-    await this.persistOnExit(token);
-
-    return new NextFlowNodeInfo(nextFlowNodeInfo, token, processTokenFacade);
+    return this._executeHandler(token, processTokenFacade, processModelFacade);
   }
 
-  public async resumeInternally(flowNodeInstance: Runtime.Types.FlowNodeInstance,
-                                processTokenFacade: IProcessTokenFacade,
-                                processModelFacade: IProcessModelFacade,
-                                identity: IIdentity,
-                              ): Promise<NextFlowNodeInfo> {
+  protected async resumeInternally(flowNodeInstance: Runtime.Types.FlowNodeInstance,
+                                   processTokenFacade: IProcessTokenFacade,
+                                   processModelFacade: IProcessModelFacade,
+                                   identity: IIdentity,
+                                  ): Promise<NextFlowNodeInfo> {
 
-    throw new Error('Not implemented yet.');
-  }
+    switch (flowNodeInstance.state) {
+      case Runtime.Types.FlowNodeInstanceState.suspended:
+        return this._continueAfterSuspend(flowNodeInstance, processTokenFacade, processModelFacade);
+      case Runtime.Types.FlowNodeInstanceState.running:
 
-  private async _registerEventHandlerAndSendMessage(token: Runtime.Types.ProcessToken): Promise<void> {
+        const resumeToken: Runtime.Types.ProcessToken =
+          flowNodeInstance.tokens.find((token: Runtime.Types.ProcessToken): boolean => {
+            return token.type === Runtime.Types.ProcessTokenType.onResume;
+          });
 
-    return new Promise<void>((resolve: Function, reject: Function): void => {
-      const messageName: string = this.sendTask.messageEventDefinition.name;
+        const receiveTaskNotYetTriggered: boolean = resumeToken === undefined;
 
-      const messageEventName: string = eventAggregatorSettings
-        .routePaths
-        .receiveTaskReached
-        .replace(eventAggregatorSettings.routeParams.messageReference, messageName);
-
-      const subscription: ISubscription = this._eventAggregator.subscribeOnce(messageEventName, async(message: MessageEventReachedMessage) => {
-
-        await this.persistOnResume(token);
-
-        if (subscription) {
-          subscription.dispose();
+        if (receiveTaskNotYetTriggered) {
+          return this._continueAfterEnter(flowNodeInstance, processTokenFacade, processModelFacade);
         }
 
-        resolve(message);
+        return this._continueAfterResume(resumeToken, processTokenFacade, processModelFacade);
+      default:
+        throw new InternalServerError(`Cannot resume ReceiveTask instance ${flowNodeInstance.id}, because it was already finished!`);
+    }
+  }
+
+  /**
+   * Resumes the given FlowNodeInstance from the point where it assumed the
+   * "onEnter" state.
+   *
+   * Basically, the handler was not yet executed, except for the initial
+   * state change.
+   *
+   * @async
+   * @param   flowNodeInstance   The FlowNodeInstance to resume.
+   * @param   processTokenFacade The ProcessTokenFacade to use for resuming.
+   * @param   processModelFacade The processModelFacade to use for resuming.
+   * @returns                    The Info for the next FlowNode to run.
+   */
+  private async _continueAfterEnter(flowNodeInstance: Runtime.Types.FlowNodeInstance,
+                                    processTokenFacade: IProcessTokenFacade,
+                                    processModelFacade: IProcessModelFacade,
+                                   ): Promise<NextFlowNodeInfo> {
+
+    // When the FNI was interrupted directly after the onEnter state change, only one token will be present.
+    const onEnterToken: Runtime.Types.ProcessToken = flowNodeInstance.tokens[0];
+
+    return this._executeHandler(onEnterToken, processTokenFacade, processModelFacade);
+  }
+
+  /**
+   * Resumes the given FlowNodeInstance from the point where it assumed the
+   * "onSuspended" state.
+   *
+   * When the FlowNodeInstance was interrupted during this stage, we need to
+   * run the handler again, except for the "onSuspend" state change.
+   *
+   * @async
+   * @param   flowNodeInstance   The FlowNodeInstance to resume.
+   * @param   processTokenFacade The ProcessTokenFacade to use for resuming.
+   * @param   processModelFacade The processModelFacade to use for resuming.
+   * @returns                    The Info for the next FlowNode to run.
+   */
+  private async _continueAfterSuspend(flowNodeInstance: Runtime.Types.FlowNodeInstance,
+                                      processTokenFacade: IProcessTokenFacade,
+                                      processModelFacade: IProcessModelFacade,
+                                     ): Promise<NextFlowNodeInfo> {
+
+    const suspendToken: Runtime.Types.ProcessToken =
+      flowNodeInstance.tokens.find((token: Runtime.Types.ProcessToken): boolean => {
+        return token.type === Runtime.Types.ProcessTokenType.onSuspend;
       });
 
-      this._sendMessage(messageName, token);
+    return this._executeHandler(suspendToken, processTokenFacade, processModelFacade);
+  }
+
+  /**
+   * Resumes the given FlowNodeInstance from the point where it assumed the
+   * "onResumed" state.
+   *
+   * Basically, the ReceiveTask was already finished.
+   * The final result is only missing in the database.
+   *
+   * @async
+   * @param   resumeToken   The FlowNodeInstance to resume.
+   * @param   processTokenFacade The ProcessTokenFacade to use for resuming.
+   * @param   processModelFacade The processModelFacade to use for resuming.
+   * @returns                    The Info for the next FlowNode to run.
+   */
+  private async _continueAfterResume(resumeToken: Runtime.Types.ProcessToken,
+                                     processTokenFacade: IProcessTokenFacade,
+                                     processModelFacade: IProcessModelFacade,
+                                    ): Promise<NextFlowNodeInfo> {
+
+    processTokenFacade.addResultForFlowNode(this.sendTask.id, resumeToken.payload);
+
+    const nextNodeAfter: Model.Base.FlowNode = processModelFacade.getNextFlowNodeFor(this.sendTask);
+
+    await this.persistOnExit(resumeToken);
+
+    return new NextFlowNodeInfo(nextNodeAfter, resumeToken, processTokenFacade);
+  }
+
+  private async _executeHandler(token: Runtime.Types.ProcessToken,
+                                processTokenFacade: IProcessTokenFacade,
+                                processModelFacade: IProcessModelFacade,
+                               ): Promise<NextFlowNodeInfo> {
+
+    return new Promise<NextFlowNodeInfo>(async(resolve: Function, reject: Function): Promise<void> => {
+
+      const onResponseReceivedCallback: Function = async(): Promise<void> => {
+
+        await this.persistOnResume(token);
+        await this.persistOnExit(token);
+
+        const nextFlowNodeInfo: NextFlowNodeInfo = await this.getNextFlowNodeInfo(token, processTokenFacade, processModelFacade);
+
+        return resolve(nextFlowNodeInfo);
+      };
+
+      this._waitForResponseFromReceiveTask(onResponseReceivedCallback);
+      this._sendMessage(token);
     });
   }
 
-  private async _sendMessage(messageName: string, token: Runtime.Types.ProcessToken): Promise<void> {
+  /**
+   * Waits for an incoming message from a ReceiveTask, acknowledging the receit of the message.
+   *
+   * @param callback The function to call upon receiving the message.
+   */
+  private _waitForResponseFromReceiveTask(callback: Function): void {
 
-    const messageEventName: string =
-      eventAggregatorSettings
-        .routePaths
-        .sendTaskReached
-        .replace(eventAggregatorSettings.routeParams.messageReference, messageName);
+    const messageName: string = this.sendTask.messageEventDefinition.name;
+
+    const messageEventName: string = eventAggregatorSettings
+      .routePaths
+      .receiveTaskReached
+      .replace(eventAggregatorSettings.routeParams.messageReference, messageName);
+
+    const subscription: ISubscription = this._eventAggregator.subscribeOnce(messageEventName, () => {
+
+      if (subscription) {
+        subscription.dispose();
+      }
+      callback();
+    });
+  }
+
+  /**
+   * Publishes the message stored in this SendTask on the EventAggregator.
+   *
+   * @param token The current process token.
+   */
+  private _sendMessage(token: Runtime.Types.ProcessToken): void {
+
+    const messageName: string = this.sendTask.messageEventDefinition.name;
+
+    const messageEventName: string = eventAggregatorSettings
+      .routePaths
+      .sendTaskReached
+      .replace(eventAggregatorSettings.routeParams.messageReference, messageName);
 
     const messageToSend: MessageEventReachedMessage = new MessageEventReachedMessage(
                                                                     messageName,
@@ -103,7 +217,5 @@ export class SendTaskHandler extends FlowNodeHandler<Model.Activities.SendTask> 
                                                                     token);
 
     this._eventAggregator.publish(messageEventName, messageToSend);
-
-    await this.persistOnSuspend(token);
   }
 }
