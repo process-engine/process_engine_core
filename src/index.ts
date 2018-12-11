163a8b75a826b2c2e6f8210f7678242762893412
ElRaptorus <christian.werner@5minds.de>
2018-12-11 16:49:32 +0100

    :sparkles: Make bluebird cancellable and use as default Promise type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+import * as Bluebird from 'bluebird';
+
+Bluebird.config({
+  cancellation: true,
+});
+
+// This only needs to be imported once. After that, all is done.
+import * as BluebirdGlobal from 'bluebird-global';
+
 export * from './iam_service_mock';
 export * from './model';
 export * from './runtime';
