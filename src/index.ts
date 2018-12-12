9cea1bfe7af43a666f8ddce03bfb852d953e267e
ElRaptorus <christian.werner@5minds.de>
2018-12-12 14:32:01 +0100

    :book: Improve some descriptive comments

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import * as Bluebird from 'bluebird';
 
+// By default, Promise-Cancellation is deactivated.
+// We need to activate it here in order to be able to use it.
 Bluebird.config({
   cancellation: true,
 });
 
-// This only needs to be imported once. After that, all is done.
+// This ensures that using Bluebird Promises as a return value for async
+// functions will not cause linter errors.
+// Only needs to be imported once. After that, using Bluebird types is safe.
 import * as BluebirdGlobal from 'bluebird-global';
 
 export * from './iam_service_mock';
