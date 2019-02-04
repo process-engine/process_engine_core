b5f61a01bce4befb7654342be1658e3599377260
ElRaptorus <christian.werner@5minds.de>
2019-02-04 17:06:48 +0100

    :sparkles: Implement FlowNodePersistenceFacade This facade is intended to encapsulate the FlowNodeStateChange operations, which are currently located in the base FlowNodeHandler.

--- src/runtime/engine/index.ts
+++ src/runtime/engine/index.ts
@@ -1,4 +1,5 @@
 export * from './execute_process_service';
+export * from './flow_node_persistence_facade';
 export * from './process_model_facade';
 export * from './process_model_facade_factory';
 export * from './process_token_facade';
