b90544fae38e9c6556c0e4064a90bb31439578de
ElRaptorus <christian.werner@5minds.de>
2019-02-05 15:53:07 +0100

    :sparkles: Implement error-match check in ErrorBoundaryEventHandler

--- src/runtime/engine/flow_node_handler/boundary_event_handlers/error_boundary_event_handler.ts
+++ src/runtime/engine/flow_node_handler/boundary_event_handlers/error_boundary_event_handler.ts
@@ -1,13 +1,41 @@
 import {InternalServerError} from '@essential-projects/errors_ts';
-import {IIdentity} from '@essential-projects/iam_contracts';
 import {
   IProcessTokenFacade,
+  Model,
   OnBoundaryEventTriggeredCallback,
   Runtime,
 } from '@process-engine/process_engine_contracts';
 
 import {BoundaryEventHandler} from './boundary_event_handler';
 export class ErrorBoundaryEventHandler extends BoundaryEventHandler {
+
+  /**
+   * Checks if the message of the given error is equal to the one attached
+   * to the BoundaryEvent model.
+   *
+   * If no error is attached to the model, then this handler can also handle
+   * the error.
+   * @param error The error to compare against the errorDefinition of the model.
+   */
+  public canHandleError(error: Error): boolean {
+
+    const errorDefinition: Model.EventDefinitions.ErrorEventDefinition = this._boundaryEventModel.errorEventDefinition;
+
+    const modelHasNoErrorDefinition: boolean = !errorDefinition || !errorDefinition.name;
+    if (modelHasNoErrorDefinition) {
+      return true;
+    }
+
+    // The Code property is optional and must only be evaluated, if the definition contains it.
+    // Also, modelled error codes need not necessarily be numbers, which prevents a cast to an Essential-Project Error.
+    // Therefore, using "any" is acceptable here.
+    const errorCodesMatch: boolean = !errorDefinition.code || errorDefinition.code === (error as any).code;
+    const errorMessageMatchesName: boolean = errorDefinition.name === error.message;
+
+    const isMatch: boolean = errorCodesMatch && errorMessageMatchesName;
+
+    return modelHasNoErrorDefinition || isMatch;
+  }
 
   public async waitForTriggeringEvent(
     onTriggeredCallback: OnBoundaryEventTriggeredCallback,
