a0e9c4a007b41f669570803293e97abb0c12ca04
ElRaptorus <christian.werner@5minds.de>
2019-02-18 14:47:20 +0100

    :fire: Remove all persistence layer components

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,2 +1,12 @@
-export * from './engine/index';
-export * from './persistence/index';
+export * from './auto_start_service';
+export * from './execute_process_service';
+export * from './flow_node_persistence_facade';
+export * from './process_model_facade';
+export * from './process_model_facade_factory';
+export * from './process_token_facade';
+export * from './process_token_facade_factory';
+export * from './resume_process_serivce';
+export * from './sub_process_model_facade';
+export * from './timer_facade';
+
+export * from './flow_node_handler/index';
