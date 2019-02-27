448940d81a7379b8f440fe1c506f04db5dfeda70
ElRaptorus <christian.werner@5minds.de>
2019-02-27 14:12:04 +0100

    :recycle: Fix some naming inconsistencies

--- src/model/parser/flow_node_parsers/activity_parsers/empty_activity_parser.ts
+++ src/model/parser/flow_node_parsers/activity_parsers/empty_activity_parser.ts
@@ -5,18 +5,18 @@ import {createActivityInstance} from './activity_factory';
 
 export function parseEmptyActivities(processData: any): Array<Model.Activities.EmptyActivity> {
 
-  const emptyTasks: Array<Model.Activities.EmptyActivity> = [];
+  const emptyActivities: Array<Model.Activities.EmptyActivity> = [];
 
-  const emptyTasksRaw: Array<any> = getModelPropertyAsArray(processData, BpmnTags.TaskElement.EmptyActivity);
+  const emptyActivitiesRaw: Array<any> = getModelPropertyAsArray(processData, BpmnTags.TaskElement.EmptyActivity);
 
-  if (!emptyTasksRaw || emptyTasksRaw.length === 0) {
+  if (!emptyActivitiesRaw || emptyActivitiesRaw.length === 0) {
     return [];
   }
 
-  for (const emptyTaskRaw of emptyTasksRaw) {
-    const emptyTask: Model.Activities.EmptyActivity = createActivityInstance(emptyTaskRaw, Model.Activities.EmptyActivity);
-    emptyTasks.push(emptyTask);
+  for (const emptyActivityRaw of emptyActivitiesRaw) {
+    const emptyActivity: Model.Activities.EmptyActivity = createActivityInstance(emptyActivityRaw, Model.Activities.EmptyActivity);
+    emptyActivities.push(emptyActivity);
   }
 
-  return emptyTasks;
+  return emptyActivities;
 }
