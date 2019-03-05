9cef8a9b31c4f84b47f9b1f1ebfcc069f675d4a0
ElRaptorus <christian.werner@5minds.de>
2019-03-05 16:06:01 +0100

    :white_check_mark: Add unit tests for getting the Lanes of a ProcesssModels FlowNode

--- test/process_model_facade_test.spec.ts
+++ test/process_model_facade_test.spec.ts
@@ -15,14 +15,74 @@ describe('ProcessModelFacade.getLaneForFlowNode', () => {
     await fixtureProvider.initialize();
   });
 
-  it('should successfully get the lane for each FlowNode of the process_engine_io_release.bpmn Diagram', async() => {
+  it('should successfully get the lane for each FlowNode of the DemoNutztierRiss Diagram, which contains multiple parallel lanes', async() => {
 
-    const processModelFilePath: string = './test/bpmns/process_engine_io_release.bpmn';
+    const processModelFilePath: string = './test/bpmns/DemoNutztierRiss.bpmn';
     const parsedProcessModel: Model.Process = await fixtureProvider.parseProcessModelFromFile(processModelFilePath);
 
     const processModelFacade: ProcessModelFacade = fixtureProvider.createProcessModelFacade(parsedProcessModel);
 
-    // TODO
-    should.exist(processModelFacade);
+    // tslint:disable-next-line:typedef
+    const expectedLanes = {
+      StartEvent_1: 'VWK',
+      VorgangErfassen: 'VWK',
+      Task_01xg9lr: 'VWK',
+      Task_00dom74: 'VET',
+      notizSchreiben: 'VET',
+      Task_1tk0lhq: 'ABL',
+      Task_1yzqmfq: 'ABL',
+      EndEvent_05uuvaq: 'ABL',
+    };
+
+    for (const flowNode of parsedProcessModel.flowNodes) {
+      const lane: Model.ProcessElements.Lane = processModelFacade.getLaneForFlowNode(flowNode.id);
+
+      const expectedLaneName: string = expectedLanes[flowNode.id];
+
+      const assertionErrorMessage: string = `Expected lane for FlowNodeId '${flowNode.id}' to be '${expectedLaneName}', but received '${lane.name}'`;
+      should(expectedLaneName).be.equal(lane.name, assertionErrorMessage);
+    }
+  });
+
+  it('hould successfully return undefined for each FlowNode of the generic_sample Diagram, which contains no lanes', async() => {
+
+    const processModelFilePath: string = './test/bpmns/generic_sample.bpmn';
+    const parsedProcessModel: Model.Process = await fixtureProvider.parseProcessModelFromFile(processModelFilePath);
+
+    const processModelFacade: ProcessModelFacade = fixtureProvider.createProcessModelFacade(parsedProcessModel);
+
+    for (const flowNode of parsedProcessModel.flowNodes) {
+      const lane: Model.ProcessElements.Lane = processModelFacade.getLaneForFlowNode(flowNode.id);
+
+      // The ProcessModel has no lanes, so the result should always be undefined
+      should(lane).be.undefined();
+    }
+  });
+
+  it('hould successfully get the lane for each FlowNode of the sublane_test Diagram, which contains multiple parallel lanes', async() => {
+
+    const processModelFilePath: string = './test/bpmns/sublane_test.bpmn';
+    const parsedProcessModel: Model.Process = await fixtureProvider.parseProcessModelFromFile(processModelFilePath);
+
+    const processModelFacade: ProcessModelFacade = fixtureProvider.createProcessModelFacade(parsedProcessModel);
+
+    // tslint:disable-next-line:typedef
+    const expectedLanes = {
+      StartEvent_1: 'LaneC',
+      ExclusiveGateway_1ax0imj: 'LaneC',
+      Task_0ukwbko: 'LaneC',
+      Task_0e8cbxl: 'LaneB',
+      EndEvent_1: 'LaneC',
+      EndEvent_2: 'LaneB',
+    };
+
+    for (const flowNode of parsedProcessModel.flowNodes) {
+      const lane: Model.ProcessElements.Lane = processModelFacade.getLaneForFlowNode(flowNode.id);
+
+      const expectedLaneName: string = expectedLanes[flowNode.id];
+
+      const assertionErrorMessage: string = `Expected lane for FlowNodeId '${flowNode.id}' to be '${expectedLaneName}', but received '${lane.name}'`;
+      should(expectedLaneName).be.equal(lane.name, assertionErrorMessage);
+    }
   });
 });
