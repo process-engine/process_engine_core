d38b57715281be43a09de0144c1772a9631b965a
ElRaptorus <christian.werner@5minds.de>
2019-03-07 16:50:02 +0100

    :white_check_mark: Add Unit tests for retrieving StartEvents

---
+++ test/process_model_facade/get_start_event_by_id.spec.ts
@@ -0,0 +1,48 @@
+import {before, describe, it} from 'mocha';
+import * as should from 'should';
+
+import {Model} from '@process-engine/process_model.contracts';
+
+import {ProcessModelFacade} from '../../src/runtime/process_model_facade';
+import {TestFixtureProvider} from '../test_fixture_provider';
+
+describe('ProcessModelFacade.getStartEventById', () => {
+
+  let fixtureProvider: TestFixtureProvider;
+
+  let processModelFacade: ProcessModelFacade;
+
+  before(async() => {
+    fixtureProvider = new TestFixtureProvider();
+    await fixtureProvider.initialize();
+
+    const processModelFilePath: string = './test/bpmns/generic_sample.bpmn';
+    const parsedProcessModel: Model.Process = await fixtureProvider.parseProcessModelFromFile(processModelFilePath);
+    processModelFacade = fixtureProvider.createProcessModelFacade(parsedProcessModel);
+  });
+
+  it('should return one StartEvent for a ProcessModel that only has one.', async() => {
+
+    const startEventId: string = 'ProcessInputEvent';
+
+    const startEvent: Model.Events.StartEvent = processModelFacade.getStartEventById(startEventId);
+
+    should(startEvent).be.instanceOf(Model.Events.StartEvent);
+    should(startEvent.id).be.equal(startEventId);
+  });
+
+  it('should throw an error when attempting to retrieve a non-existing StartEvent.', async() => {
+
+    const startEventId: string = 'SomeNonExistingStartEvent';
+
+    try {
+      const startEvent: Model.Events.StartEvent = processModelFacade.getStartEventById(startEventId);
+      should.fail(startEvent, undefined, 'This should have caused an error, because the StartEvent does not exist!');
+    } catch (error) {
+      const expectedErrorMessage: RegExp = /not found/i;
+      const expectedErrorCode: number = 404;
+      should(error.message).be.match(expectedErrorMessage);
+      should(error.code).be.equal(expectedErrorCode);
+    }
+  });
+});
