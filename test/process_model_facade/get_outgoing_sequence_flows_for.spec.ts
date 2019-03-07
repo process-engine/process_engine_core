3ec66e1a2c921e8e475e778c13356f1910da3bdb
ElRaptorus <christian.werner@5minds.de>
2019-03-07 15:30:56 +0100

    :white_check_mark: Add tests for retrieving SequenceFlows from the ProcessModelFacade

---
+++ test/process_model_facade/get_outgoing_sequence_flows_for.spec.ts
@@ -0,0 +1,66 @@
+import {before, describe, it} from 'mocha';
+import * as should from 'should';
+
+import {Model} from '@process-engine/process_model.contracts';
+
+import {ProcessModelFacade} from '../../src/runtime/process_model_facade';
+import {TestFixtureProvider} from '../test_fixture_provider';
+
+// tslint:disable:no-magic-numbers
+describe('ProcessModelFacade.getOutgoingSequenceFlowsFor', () => {
+
+  let fixtureProvider: TestFixtureProvider;
+
+  const processModelFilePath: string = './test/bpmns/process_engine_io_release.bpmn';
+  let processModelFacade: ProcessModelFacade;
+
+  before(async() => {
+    fixtureProvider = new TestFixtureProvider();
+    await fixtureProvider.initialize();
+
+    const parsedProcessModel: Model.Process = await fixtureProvider.parseProcessModelFromFile(processModelFilePath);
+    processModelFacade = fixtureProvider.createProcessModelFacade(parsedProcessModel);
+  });
+
+  it('should correctly return the outgoing SequenceFlows for a FlowNode with a single outgoing SequenceFlow.', async() => {
+
+    const flowNodeId: string = 'Task_1tfjjzx';
+    const expectedSequenceFlowId: string = 'SequenceFlow_0uaexrv';
+
+    const sequenceFlows: Array<Model.ProcessElements.SequenceFlow> = processModelFacade.getOutgoingSequenceFlowsFor(flowNodeId);
+
+    should(sequenceFlows).be.instanceOf(Array);
+    should(sequenceFlows.length).be.equal(1);
+    should(sequenceFlows[0].id).be.equal(expectedSequenceFlowId);
+  });
+
+  it('should correctly return the outgoing SequenceFlows for a FlowNode with multiple outgoing SequenceFlows.', async() => {
+
+    const flowNodeId: string = 'ParallelSplitGateway_1';
+
+    const expectedSequenceFlowIds: Array<string> = [
+      'SequenceFlow_1nt9fw9',
+      'SequenceFlow_1vprubq',
+      'SequenceFlow_17awqho',
+    ];
+
+    const sequenceFlows: Array<Model.ProcessElements.SequenceFlow> = processModelFacade.getOutgoingSequenceFlowsFor(flowNodeId);
+
+    should(sequenceFlows).be.instanceOf(Array);
+    should(sequenceFlows.length).be.equal(3);
+
+    for (const sequenceFlow of sequenceFlows) {
+      should(expectedSequenceFlowIds).containEql(sequenceFlow.id);
+    }
+  });
+
+  it('should return an empty Array for a FlowNode that doesn\'t have any outgoing SequenceFlows.', async() => {
+
+    const flowNodeId: string = 'EndEvent_0y6uwzm';
+
+    const sequenceFlows: Array<Model.ProcessElements.SequenceFlow> = processModelFacade.getOutgoingSequenceFlowsFor(flowNodeId);
+
+    should(sequenceFlows).be.instanceOf(Array);
+    should(sequenceFlows.length).be.equal(0);
+  });
+});
