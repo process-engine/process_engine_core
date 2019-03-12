67d39e4e553389b7c8d06d2b8fd99e1effbe5f53
ElRaptorus <christian.werner@5minds.de>
2019-03-12 14:00:40 +0100

    :recycle: Move facades to subfolder

---
+++ src/runtime/facades/index.ts
@@ -0,0 +1,7 @@
+export * from './flow_node_persistence_facade';
+export * from './process_model_facade';
+export * from './process_model_facade_factory';
+export * from './process_token_facade';
+export * from './process_token_facade_factory';
+export * from './sub_process_model_facade';
+export * from './timer_facade';
--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,12 +1,6 @@
+export * from './facades/index';
+export * from './flow_node_handler/index';
+
 export * from './auto_start_service';
 export * from './execute_process_service';
-export * from './flow_node_persistence_facade';
-export * from './process_model_facade';
-export * from './process_model_facade_factory';
-export * from './process_token_facade';
-export * from './process_token_facade_factory';
 export * from './resume_process_serivce';
-export * from './sub_process_model_facade';
-export * from './timer_facade';
-
-export * from './flow_node_handler/index';
