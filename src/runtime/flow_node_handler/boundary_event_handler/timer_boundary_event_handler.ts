59f25698ccd61c6a76789a1243d56f45ae2d2372
ElRaptorus <christian.werner@5minds.de>
2020-01-28 15:11:51 +0100

    :sparkles: Implement resumption to TimerBoundaryEventHandler

--- src/runtime/flow_node_handler/boundary_event_handler/timer_boundary_event_handler.ts
+++ src/runtime/flow_node_handler/boundary_event_handler/timer_boundary_event_handler.ts
@@ -39,14 +39,44 @@ export class TimerBoundaryEventHandler extends BoundaryEventHandler {
     attachedFlowNodeInstanceId: string,
   ): Promise<void> {
 
-    this.logger.verbose(`Initializing TimerBoundaryEvent for ProcessModel ${token.processModelId} in ProcessInstance ${token.processInstanceId}`);
+    this.logger.verbose(`Initializing BoundaryEvent on FlowNodeInstance ${attachedFlowNodeInstanceId} in ProcessInstance ${token.processInstanceId}`);
     this.attachedFlowNodeInstanceId = attachedFlowNodeInstanceId;
+
+    await this.persistOnEnter(token);
+
+    this.initializeTimer(onTriggeredCallback, token, processTokenFacade, processModelFacade);
+  }
+
+  public async resumeWait(
+    onTriggeredCallback: OnBoundaryEventTriggeredCallback,
+    token: ProcessToken,
+    processTokenFacade: IProcessTokenFacade,
+    processModelFacade: IProcessModelFacade,
+    attachedFlowNodeInstanceId: string,
+  ): Promise<void> {
+
+    this.logger.verbose(`Resuming BoundaryEvent on FlowNodeInstance ${attachedFlowNodeInstanceId} in ProcessInstance ${token.processInstanceId}`);
+    this.attachedFlowNodeInstanceId = attachedFlowNodeInstanceId;
+
+    this.initializeTimer(onTriggeredCallback, token, processTokenFacade, processModelFacade);
+  }
+
+  public async cancel(token: ProcessToken, processModelFacade: IProcessModelFacade): Promise<void> {
+    this.timerFacade.cancelTimerSubscription(this.timerSubscription);
+    await super.cancel(token, processModelFacade);
+  }
+
+  private initializeTimer(
+    onTriggeredCallback: OnBoundaryEventTriggeredCallback,
+    token: ProcessToken,
+    processTokenFacade: IProcessTokenFacade,
+    processModelFacade: IProcessModelFacade,
+  ): void {
 
     const laneContainingCurrentFlowNode = processModelFacade.getLaneForFlowNode(this.boundaryEventModel.id);
     if (laneContainingCurrentFlowNode != undefined) {
       token.currentLane = laneContainingCurrentFlowNode.name;
     }
-    await this.persistOnEnter(token);
 
     const timerElapsed = async (): Promise<void> => {
 
@@ -69,11 +99,6 @@ export class TimerBoundaryEventHandler extends BoundaryEventHandler {
     this.timerSubscription = this
       .timerFacade
       .initializeTimer(this.boundaryEventModel, this.boundaryEventModel.timerEventDefinition, processTokenFacade, timerElapsed);
-  }
-
-  public async cancel(token: ProcessToken, processModelFacade: IProcessModelFacade): Promise<void> {
-    this.timerFacade.cancelTimerSubscription(this.timerSubscription);
-    await super.cancel(token, processModelFacade);
   }
 
 }
