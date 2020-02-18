87d3ce00d2073a547c618874653e37b29ddd93ca
ElRaptorus <christian.werner@5minds.de>
2020-02-18 14:53:46 +0100

    :fire: Remove obsolete timeout workaround The async locks for the gateway render this workaround obsolete

--- src/runtime/flow_node_handler/activity_handler/activity_handler.ts
+++ src/runtime/flow_node_handler/activity_handler/activity_handler.ts
@@ -91,17 +91,6 @@ export abstract class ActivityHandler<TFlowNode extends Model.Base.FlowNode> ext
               identity,
             );
             nextFlowNodeExecutionPromises.push(handleNextFlowNodePromise);
-
-            // NOTE:
-            // This is a workaround for a problem with the resumption of multiple parallel branches that were executed right up to the JoinGateway.
-            // When multiple branches arrive at the JoinGateway at the EXACT same moment, it is possible
-            // that multiple instances for that same Gateway are created.
-            // Since the Gateway always waits for ALL incoming branches before moving on,
-            // this will result in the process instance getting stuck forever.
-            // Using a timeout helps us to get around this issue, but it is just a hacky workaround. We need a more permanent solution for this.
-            if (nextFlowNodes.length > 1) {
-              await new Promise((cb): NodeJS.Timeout => setTimeout(cb, 33));
-            }
           }
 
           await Promise.all(nextFlowNodeExecutionPromises);
@@ -190,11 +179,6 @@ export abstract class ActivityHandler<TFlowNode extends Model.Base.FlowNode> ext
               allFlowNodeInstances,
             );
             nextFlowNodeExecutionPromises.push(handleNextFlowNodePromise);
-
-            // See above
-            if (nextFlowNodes.length > 1) {
-              await new Promise((cb): NodeJS.Timeout => setTimeout(cb, 33));
-            }
           }
 
           await Promise.all(nextFlowNodeExecutionPromises);
