7208fb9dd1fdca33d227e393942f92ffcab38813
ElRaptorus <christian.werner@5minds.de>
2020-03-19 12:44:34 +0100

    :loud_sound: Add logging to BoundaryEventHandlers

--- src/runtime/flow_node_handler/boundary_event_handler/signal_boundary_event_handler.ts
+++ src/runtime/flow_node_handler/boundary_event_handler/signal_boundary_event_handler.ts
@@ -1,7 +1,10 @@
-import {Subscription} from '@essential-projects/event_aggregator_contracts';
+import {Logger} from 'loggerhythm';
 
-import {FlowNodeInstance, ProcessToken} from '@process-engine/persistence_api.contracts';
+import {IEventAggregator, Subscription} from '@essential-projects/event_aggregator_contracts';
+
+import {FlowNodeInstance, Model, ProcessToken} from '@process-engine/persistence_api.contracts';
 import {
+  IFlowNodePersistenceFacade,
   IProcessModelFacade,
   IProcessTokenFacade,
   OnBoundaryEventTriggeredCallback,
@@ -13,7 +16,18 @@ import {BoundaryEventHandler} from './boundary_event_handler';
 
 export class SignalBoundaryEventHandler extends BoundaryEventHandler {
 
+  private readonly logger: Logger;
+
   private subscription: Subscription;
+
+  constructor(
+    eventAggregator: IEventAggregator,
+    flowNodePersistenceFacade: IFlowNodePersistenceFacade,
+    boundaryEventModel: Model.Events.BoundaryEvent,
+  ) {
+    super(eventAggregator, flowNodePersistenceFacade, boundaryEventModel);
+    this.logger = new Logger(`processengine:timer_boundary_event_handler:${boundaryEventModel.id}`);
+  }
 
   public async waitForTriggeringEvent(
     onTriggeredCallback: OnBoundaryEventTriggeredCallback,
@@ -23,6 +37,7 @@ export class SignalBoundaryEventHandler extends BoundaryEventHandler {
     attachedFlowNodeInstanceId: string,
   ): Promise<void> {
 
+    this.logger.verbose(`Initializing BoundaryEvent on FlowNodeInstance ${attachedFlowNodeInstanceId} in ProcessInstance ${token.processInstanceId}`);
     await this.persistOnEnter(token, attachedFlowNodeInstanceId);
 
     this.waitForSignal(onTriggeredCallback, token, processModelFacade);
@@ -36,6 +51,8 @@ export class SignalBoundaryEventHandler extends BoundaryEventHandler {
     processModelFacade: IProcessModelFacade,
     attachedFlowNodeInstanceId: string,
   ): Promise<void> {
+
+    this.logger.verbose(`Resuming BoundaryEvent on FlowNodeInstance ${attachedFlowNodeInstanceId} in ProcessInstance ${token.processInstanceId}`);
 
     this.boundaryEventInstance = boundaryEventInstance;
     this.attachedFlowNodeInstanceId = attachedFlowNodeInstanceId;
